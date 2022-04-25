f3110e90b845609f9277fe37a3d0d4d263e4d45d
Babin Ion <ionbabin123@mail.ru>
2022-04-25 12:52:06 +0300

    added something to doom config idk

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -25,6 +25,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-homage-black)
+(setq doom-font "Iosevka Term SS13")
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
