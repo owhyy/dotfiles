6b90cf95a7cb2bc6ccfbaaf4de2816907ec25854
Babin Ion <ionbabin123@mail.ru>
2022-06-12 01:49:47 +0300

    user-overrides for arkenfox

---
+++ user-overrides.js
@@ -0,0 +1,7 @@
+user_pref("keyword.enabled", true); // 0801 - enable location bar using search
+
+/* 2811 - set/enforce what items to clear on shutdown */
+user_pref("privacy.clearOnShutdown.cache", true);
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.clearOnShutdown.cookies", false);
+user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504 disable letterboxing
