5bcd421b7d9f3bc62c9b4a306fa611f14117176e
owhyy <ionbabin123@mail.ru>
2022-07-03 14:29:39 +0300

    added boxdraw patch

--- .local/src/st/config.h
+++ .local/src/st/config.h
@@ -5,8 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "LiterationMono Nerd Font Mono:pixelsize=12:antialias=true:autohint=true";
+//static char *font = "LiterationMono Nerd Font Mono:pixelsize=12:antialias=true:autohint=true";
 //static char *font = "mononoki Nerd Font Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Iosevka Term,Iosevka Nerd Font Mono:pixelsize=14:style=Regular:autohint=true";
 
 static int borderpx = 1;
 
@@ -68,6 +69,18 @@ static unsigned int blinktimeout = 800;
  * thickness of underline and bar cursors
  */
 static unsigned int cursorthickness = 2;
+
+/*
+ * 1: render most of the lines/blocks characters without using the font for
+ *    perfect alignment between cells (U2500 - U259F except dashes/diagonals).
+ *    Bold affects lines thickness if boxdraw_bold is not 0. Italic is ignored.
+ * 0: disable (render all U25XX glyphs normally from the font).
+ */
+const int boxdraw = 0;
+const int boxdraw_bold = 0;
+
+/* braille (U28XX):  1: render as adjacent "pixels",  0: use font */
+const int boxdraw_braille = 0;
 
 /*
  * bell volume. It must be a value between -100 and 100. Use 0 for disabling
