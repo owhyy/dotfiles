bdd80bf820ae969405552d305b903eac937ddcb5
Babin Ion <ionbabin123@mail.ru>
2022-07-06 22:21:40 +0300

    added html&css langservers

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -12,8 +12,8 @@ vim.cmd('command! CleanSwap !rm -rf ~/.local/share/nvim/swap/')
 vim.cmd("command! HelpTags Telescope help_tags")
 
 -- autocompile dwm and dwmblocks when saving (not working for some reason :( )
-vim.cmd [[autocmd BufWritePost ~/.local/src/dwm/config.h !cd ~/.local/src/dwm/; sudo make install && {killall -q dwm;setsid -f dwm}]]
-vim.cmd [[autocmd BufWritePost ~/.local/src/dwmblocks/blocks.h !cd ~/.local/src/dwmblocks/; sudo make install && {killall -q dwmblocks;setsid -f dwmblocks}]]
+-- vim.cmd [[autocmd BufWritePost ~/.local/src/dwm/config.h !cd ~/.local/src/dwm/; sudo make install && {killall -q dwm;setsid -f dwm}]]
+-- vim.cmd [[autocmd BufWritePost ~/.local/src/dwmblocks/blocks.h !cd ~/.local/src/dwmblocks/; sudo make install && {killall -q dwmblocks;setsid -f dwmblocks}]]
 
 -- fpc complier autorun
 vim.cmd [[autocmd BufWritePost *.pas :FPC]]
